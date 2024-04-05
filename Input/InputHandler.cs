0729edeb806da4eb3d7ace10784cea6d80a8a7c3
Vladislav Sidorovici <sidorvla@fit.cvut.cz>
2024-04-06 00:05:11 +0200

    Adding InputHandler.

---
+++ Input/InputHandler.cs
@@ -0,0 +1,26 @@
+﻿namespace SeaBattles.Console.Input
+{
+	internal class InputHandler
+	{
+		private readonly List<InputToken> _tokens = new();
+
+		public void Add(InputToken token)
+		{
+			_tokens.Add(token);
+		}
+
+		public void Add(string regex, Action<string> action)
+		{
+			Add(new InputToken(regex, action));
+		}
+
+		public void Handle(string input)
+		{
+			foreach (var token in _tokens)
+			{
+				if (token.DoIfMatch(input))
+					return;
+			}
+		}
+	}
+}
---
+++ Input/InputToken.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+
+namespace SeaBattles.Console.Input
+{
+	internal class InputToken
+	{
+		private readonly Regex _regex;
+
+		public Action<string> Action { get; }
+
+		public InputToken(string regex, Action<string> action)
+		{
+			_regex = new Regex(regex);
+			Action = action;
+		}
+
+		public bool DoIfMatch(string input)
+		{
+			if (!_regex.IsMatch(input))
+				return false;
+
+			Action.Invoke(input);
+
+			return true;
+		}
+	}
+}
